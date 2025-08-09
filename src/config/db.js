cb183201d613ba441228839d617f73d1fd5201b2
fachrimartsa <fachrimartsa1164@gmail.com>
2025-08-10 03:13:07 +0700

    PASS DB

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const postgres = require('postgres');
 
-const connectionString = 'postgresql://postgres:fachri1164@aws-0-us-east-2.pooler.supabase.com:6543/postgres';
+const connectionString = 'postgresql://postgres:@Fachri1164@aws-0-us-east-2.pooler.supabase.com:6543/postgres';
 
 const sql = postgres(connectionString, {
   ssl: { rejectUnauthorized: false }
