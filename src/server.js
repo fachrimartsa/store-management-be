59f3fedcf17a25db24db28f411167cf7a42d7173
fachrimartsa <fachrimartsa1164@gmail.com>
2025-08-10 03:50:46 +0700

    AKSES

--- src/server.js
+++ src/server.js
@@ -9,13 +9,7 @@ const app = express();
 const allowedOrigins = ['http://localhost:5173', 'http://localhost:4000', 'https://fachrimartsa.github.io/store-management-fe/'];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*' ,
   credentials: true
 }));
 
