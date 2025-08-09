4d1386e8062aaa720cb52f7d88f6af25732480df
fachrimartsa <fachrimartsa1164@gmail.com>
2025-08-10 03:00:39 +0700

    DB ANJGGG

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const postgres = require('postgres');
 
-const connectionString = 'postgresql://postgres:fachri1164@db.lyqpmcszxykrnnsljkkf.supabase.co:5432/postgres';
+const connectionString = 'postgresql://postgres:fachri1164@aws-0-us-east-2.pooler.supabase.com:6543/postgres';
 
 const sql = postgres(connectionString, {
   ssl: { rejectUnauthorized: false }
