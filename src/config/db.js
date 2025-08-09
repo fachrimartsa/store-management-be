3d9c92b2afc1c16c96a0cf3e9bad44d4e736768b
fachrimartsa <fachrimartsa1164@gmail.com>
2025-08-10 02:55:27 +0700

    DBBBB

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
 const postgres = require('postgres');
 
-const sql = postgres('postgresql://postgres.lyqpmcszxykrnnsljkkf:YOUR_PASSWORD@aws-0-us-east-2.pooler.supabase.com:6543/postgres', {
-  ssl: {
-    rejectUnauthorized: false
-  }
+const connectionString = 'postgresql://postgres:fachri1164@db.lyqpmcszxykrnnsljkkf.supabase.co:5432/postgres';
+
+const sql = postgres(connectionString, {
+  ssl: { rejectUnauthorized: false }
 });
 
-export default sql;
+module.exports = sql;
