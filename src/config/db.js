50cb1a57e7ba1bb75a81158edd3111c4cda9846b
fachrimartsa <fachrimartsa1164@gmail.com>
2025-08-10 03:19:52 +0700

    DB TOLOL

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
-const postgres = require('postgres');
+const postgres = require('postgres')
 
-const connectionString = 'postgresql://postgres:@Fachri1164@aws-0-us-east-2.pooler.supabase.com:6543/postgres';
+const connectionString = 'postgresql://postgres.lyqpmcszxykrnnsljkkf:YOUR_PASSWORD@aws-0-us-east-2.pooler.supabase.com:6543/postgres'
 
 const sql = postgres(connectionString, {
   ssl: { rejectUnauthorized: false }
-});
+})
 
-module.exports = sql;
+module.exports = sql
