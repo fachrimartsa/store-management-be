ab1d01b4b8bdc256bd2b1c2ac4f58c90a39a2a6a
fachrimartsa <fachrimartsa1164@gmail.com>
2025-08-10 02:37:25 +0700

    DB

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 import postgres from 'postgres';
 
-const connectionString = 'postgresql://postgres:fachri1164@host:port/database';
+const connectionString = 'postgresql://postgres:fachri1164@db.lyqpmcszxykrnnsljkkf.supabase.co:5432/postgres';
 
 const sql = postgres(connectionString, {
   ssl: { rejectUnauthorized: false }
