79a9ad2b80bd5abd81de5537d9b964d5e76e93d8
fachrimartsa <fachrimartsa1164@gmail.com>
2025-08-10 03:20:38 +0700

    DB TOLOLLLL

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const postgres = require('postgres')
 
-const connectionString = 'postgresql://postgres.lyqpmcszxykrnnsljkkf:YOUR_PASSWORD@aws-0-us-east-2.pooler.supabase.com:6543/postgres'
+const connectionString = 'postgresql://postgres.lyqpmcszxykrnnsljkkf:@Fachri1164@aws-0-us-east-2.pooler.supabase.com:6543/postgres'
 
 const sql = postgres(connectionString, {
   ssl: { rejectUnauthorized: false }
