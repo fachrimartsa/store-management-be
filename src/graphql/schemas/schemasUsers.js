6480df9e8a54ab837fc074cb8bca00a3c98ade78
fachrimartsa <fachrimartsa1164@gmail.com>
2025-08-24 17:18:19 +0700

    Phase 1

--- src/graphql/schemas/schemasUsers.js
+++ src/graphql/schemas/schemasUsers.js
@@ -5,11 +5,11 @@ const schemaUsers = `
         usr_username: String!
     }
 
-    extend type Query {
+    type Query {
         getUserById(usr_id: ID!): Users
     }
 
-    extend type Mutation {
+    type Mutation {
         loginUser(
             usr_username: String!
             usr_password: String!
